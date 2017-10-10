ec4130f91857e66cf5e732de1d7b2b80c31157f3
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-10 12:30:15 -0700

    Split condition into two rows. #1

--- public/main.js
+++ public/main.js
@@ -20,8 +20,21 @@ const renderForm = () => {
       createElement('div', { class: 'row' }, [ 'Title:',
         createElement('input', { class: 'title' }, [])
       ]),
-      createElement('div', { class: 'row' }, [ 'Condition:',
-        createElement('select', { name: 'condition' }, [
+      createElement('div', { class: 'row' }, [ 'Vinyl Condition:',
+        createElement('select', { name: 'vinylCondition' }, [
+          createElement('option', { value: 'SS' }, ['SS']),
+          createElement('option', { value: 'NM' }, ['NM']),
+          createElement('option', { value: 'VG+' }, ['VG+']),
+          createElement('option', { value: 'VG' }, ['VG']),
+          createElement('option', { value: 'VG-' }, ['VG-']),
+          createElement('option', { value: 'G+' }, ['G+']),
+          createElement('option', { value: 'G' }, ['G']),
+          createElement('option', { value: 'F' }, ['F']),
+          createElement('option', { value: 'P' }, ['P'])
+        ])
+      ]),
+      createElement('div', { class: 'row' }, [ 'Cover Condition:',
+        createElement('select', { name: 'coverCondition' }, [
           createElement('option', { value: 'SS' }, ['SS']),
           createElement('option', { value: 'NM' }, ['NM']),
           createElement('option', { value: 'VG+' }, ['VG+']),
