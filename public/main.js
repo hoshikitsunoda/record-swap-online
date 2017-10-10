45d16349bdf4f3075cf5312ea612c349a01ca2bd
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-10 12:24:53 -0700

    Render a form for new record listing. #1

---
+++ public/main.js
@@ -0,0 +1,54 @@
+const createElement = (tagName, attributes, children) => {
+  const $element = document.createElement(tagName)
+  for (let key in attributes) {
+    $element.setAttribute(key, attributes[key])
+  }
+  children.forEach(child => {
+    child instanceof Node
+      ? $element.appendChild(child)
+      : $element.appendChild(document.createTextNode(child))
+  })
+  return $element
+}
+
+const renderForm = () => {
+  const $form =
+    createElement('div', { class: 'container' }, [
+      createElement('div', { class: 'row' }, [ 'Artist:',
+        createElement('input', { class: 'artist' }, [])
+      ]),
+      createElement('div', { class: 'row' }, [ 'Title:',
+        createElement('input', { class: 'title' }, [])
+      ]),
+      createElement('div', { class: 'row' }, [ 'Condition:',
+        createElement('select', { name: 'condition' }, [
+          createElement('option', { value: 'SS' }, ['SS']),
+          createElement('option', { value: 'NM' }, ['NM']),
+          createElement('option', { value: 'VG+' }, ['VG+']),
+          createElement('option', { value: 'VG' }, ['VG']),
+          createElement('option', { value: 'VG-' }, ['VG-']),
+          createElement('option', { value: 'G+' }, ['G+']),
+          createElement('option', { value: 'G' }, ['G']),
+          createElement('option', { value: 'F' }, ['F']),
+          createElement('option', { value: 'P' }, ['P'])
+        ])
+      ]),
+      createElement('div', { class: 'row' }, [ 'Format:',
+        createElement('select', { class: 'Format' }, [
+          createElement('option', { value: 'LP' }, ['LP']),
+          createElement('option', { value: 'EP' }, ['EP']),
+          createElement('option', { value: '7"' }, ['7"'])
+        ])
+      ]),
+      createElement('div', { class: 'row' }, [ 'Year:',
+        createElement('input', { class: 'year' }, [])
+      ]),
+      createElement('div', { class: 'row' }, [ 'Label:',
+        createElement('input', { class: 'label' }, [])
+      ]),
+      createElement('button', { type: 'submit', class: 'submit' }, ['SUBMIT'])
+    ])
+  return $form
+}
+
+document.body.appendChild(renderForm())
