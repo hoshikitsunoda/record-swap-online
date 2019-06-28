99818f61b7534610c9a69910a04d8bd7f802c618
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-06-28 16:59:36 -0700

    receives props and renders data on page #17

--- client/src/ItemPanel.js
+++ client/src/ItemPanel.js
@@ -1,17 +1,26 @@
 import React from 'react'
 
-const ItemPanel = props => (
-  <div>
-    <img src="" alt="" className="item_photo" />
-    <div className="description">
-      <ul>
-        <li>Title</li>
-        <li>Artist</li>
-        <li>Condition</li>
-        <li>Price</li>
-      </ul>
+const ItemPanel = props => {
+  const path = 'http://localhost:3000/'
+  return (
+    <div>
+      {props.recordInfo.map((record, i) => (
+        <div key={i}>
+          <img src={path + record.filename} alt="" className="item_photo" />
+          <div className="description">
+            <ul>
+              <li>{record.title}</li>
+              <li>{record.artist}</li>
+              <li>
+                {record.mediaCondition}/{record.coverCondition}
+              </li>
+              <li>{record.price}</li>
+            </ul>
+          </div>
+        </div>
+      ))}
     </div>
-  </div>
-)
+  )
+}
 
 export default ItemPanel
