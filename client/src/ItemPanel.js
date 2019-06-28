78012990519996a507a23508346974673e74fa38
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-06-28 16:12:03 -0700

    created ItemPanel component #17

---
+++ client/src/ItemPanel.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ItemPanel = props => (
+  <div>
+    <img src="" alt="" className="item_photo" />
+    <div className="description">
+      <ul>
+        <li>Title</li>
+        <li>Artist</li>
+        <li>Condition</li>
+        <li>Price</li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default ItemPanel
