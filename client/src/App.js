68b8d0017c3ca0d398719aac3b19259d468c6fa0
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-01 16:12:48 -0700

    imported theme from material ui #17

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,11 @@ import axios from 'axios'
 
 import ItemPanel from './components/ItemPanel'
 
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from 'styled-components'
 import * as Styled from './components/styled'
 
+const theme = createMuiTheme()
 class App extends Component {
   state = {
     data: []
@@ -24,10 +27,12 @@ class App extends Component {
   }
   render() {
     return (
-      <Styled.App className="App">
-        <Styled.GlobalStyle />
-        <ItemPanel recordInfo={this.state.data} />
-      </Styled.App>
+      <ThemeProvider theme={theme}>
+        <Styled.App className="App">
+          <Styled.GlobalStyle />
+          <ItemPanel recordInfo={this.state.data} />
+        </Styled.App>
+      </ThemeProvider>
     )
   }
 }
