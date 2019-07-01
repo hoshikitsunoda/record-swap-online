b0b2a452096ccff5ac409dd8c196bd53d118e5ea
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-06-30 23:31:25 -0700

    imported InfoBox in ItemPanel #17

--- client/src/components/ItemPanel.js
+++ client/src/components/ItemPanel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import InfoBox from './InfoBox'
 
 const ItemPanel = props => {
   const path = 'http://localhost:3000/'
@@ -8,14 +9,7 @@ const ItemPanel = props => {
         <div key={i}>
           <img src={path + record.filename} alt="" className="item_photo" />
           <div className="description">
-            <ul>
-              <li>{record.title}</li>
-              <li>{record.artist}</li>
-              <li>
-                {record.mediaCondition}/{record.coverCondition}
-              </li>
-              <li>{record.price}</li>
-            </ul>
+            <InfoBox info={record} />
           </div>
         </div>
       ))}
