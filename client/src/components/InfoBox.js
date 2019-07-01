be6cba6ee17fc39a42ee9d821cca92f95f7c41bb
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-01 14:02:25 -0700

    removed console.log #17

--- client/src/components/InfoBox.js
+++ client/src/components/InfoBox.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
-const InfoBox = props => {
-  props = props.info
+const InfoBox = ({ info }) => {
+  const path = 'http://localhost:3000/'
   return (
-    <React.Fragment>
-      <ul>
-        <li>{props.title}</li>
-        <li>{props.artist}</li>
-        <li>
-          {props.mediaCondition}/{props.coverCondition}
-        </li>
-        <li>{props.price}</li>
-      </ul>
-    </React.Fragment>
+    <div>
+      <img src={path + info.filename} alt="" className="itemPhoto" />
+      <div className="info">
+        <ul>
+          <li>{info.title}</li>
+          <li>{info.artist}</li>
+          <li>
+            {info.mediaCondition}/{info.coverCondition}
+          </li>
+          <li>{info.price}</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/ItemPanel.js
+++ client/src/components/ItemPanel.js
@@ -2,16 +2,10 @@ import React from 'react'
 import InfoBox from './InfoBox'
 
 const ItemPanel = props => {
-  const path = 'http://localhost:3000/'
   return (
     <React.Fragment>
       {props.recordInfo.map((record, i) => (
-        <div key={i}>
-          <img src={path + record.filename} alt="" className="item_photo" />
-          <div className="description">
-            <InfoBox info={record} />
-          </div>
-        </div>
+        <InfoBox info={record} key={i} />
       ))}
     </React.Fragment>
   )
