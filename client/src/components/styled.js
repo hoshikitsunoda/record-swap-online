d5777a852a5c5e4a687bda132a08ec7e49009df2
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-01 14:17:03 -0700

    created styled.js #17

---
+++ client/src/components/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+// InfoBox styling
+
+export const UnorderedList = styled.ul`
+  list-style: none;
+`
+
+export const Image = styled.img`
+  width: 100%;
+`
+
+export const InfoBox = styled.div`
+  padding: 1em;
+`
+
+// ItemPanel styling
+
+export const ItemPanel = styled.div`
+  width: 100%;
+`
