4950801b0055853425f253a6bf02794b2b5f9365
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-05 01:45:32 -0700

    changed InfoBox to stateless coponent #19

--- client/src/components/InfoBox.js
+++ client/src/components/InfoBox.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import * as Styled from './styled'
 
-class InfoBox extends Component {
-  state = { isShown: true }
-  render() {
-    const { info } = this.props
-    const path = 'http://localhost:3000/'
-    return (
-      <Styled.InfoBox>
-        <Styled.ImageContainer>
-          <img
-            src={path + info.filename}
-            data-id={info._id}
-            alt={info.title}
-            className="itemPhoto"
-          />
-        </Styled.ImageContainer>
-        <Styled.InfoContainer>
-          <Styled.UnorderedList>
-            <li>{info.title}</li>
-            <li>{info.artist}</li>
-            <li>
-              {info.mediaCondition}/{info.coverCondition}
-            </li>
-            <li>${info.price}USD</li>
-          </Styled.UnorderedList>
-          <Styled.DetailButtonContainer>
-            <button>
-              <a href="JavaScript:;">View Detail</a>
-            </button>
-          </Styled.DetailButtonContainer>
-        </Styled.InfoContainer>
-      </Styled.InfoBox>
-    )
-  }
+const InfoBox = props => {
+  const { info } = props
+  const path = 'http://localhost:3000/'
+  return (
+    <Styled.InfoBox className="infoBox">
+      <Styled.ImageContainer>
+        <img
+          src={path + info.filename}
+          data-id={info._id}
+          alt={info.title}
+          className="itemPhoto"
+        />
+      </Styled.ImageContainer>
+      <Styled.InfoContainer>
+        <Styled.UnorderedList>
+          <li>{info.title}</li>
+          <li>{info.artist}</li>
+          <li>
+            {info.mediaCondition}/{info.coverCondition}
+          </li>
+          <li>${info.price}USD</li>
+        </Styled.UnorderedList>
+        <Styled.DetailButtonContainer>
+          <button>
+            <a href="JavaScript:;">View Detail</a>
+          </button>
+        </Styled.DetailButtonContainer>
+      </Styled.InfoContainer>
+    </Styled.InfoBox>
+  )
 }
 
 export default InfoBox
