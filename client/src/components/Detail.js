fb7e4ea30b7c209c588467503bb779dc448ea7ca
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-05 03:01:28 -0700

    Detail component now receives data as props #19

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Detail = () => {
+const Detail = props => {
+  console.log(props)
   return (
     <div>
       <p>Detail Here</p>
--- client/src/components/ItemPanel.js
+++ client/src/components/ItemPanel.js
@@ -10,11 +10,12 @@ const ItemPanel = props => {
   const ListView = props.recordInfo.map((record, i) => (
     <InfoBox info={record} key={i} />
   ))
+  const DetailView = <Detail {...props} />
   return (
     <Styled.ItemPanel className="itemPanel">
       <Router>
         <Route path="/" exact render={() => ListView} />
-        <Route path="/detail" render={() => Detail()} />
+        <Route path="/detail" render={() => DetailView} />
       </Router>
     </Styled.ItemPanel>
   )
