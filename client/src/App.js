bd5fb6828be87271733906b7050905c7e2719231
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-06 01:59:07 -0700

    wrapped App in BrowserRouter #19

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import { createMuiTheme } from '@material-ui/core/styles'
 import { ThemeProvider } from 'styled-components'
 import * as Styled from './components/styled'
 
+import { BrowserRouter } from 'react-router-dom'
+
 const theme = createMuiTheme()
 class App extends Component {
   state = {
@@ -17,16 +19,20 @@ class App extends Component {
     // const res = await axios.get('http://localhost:3000/inventory/')
     const res = await axios.get(url)
     const { data } = await res
-    this.setState({ data: data.data.reverse() })
+    this.setState({
+      data: data.data.length < 1 ? data.data.reverse() : data.data
+    })
   }
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Styled.App className="App">
-          <Styled.GlobalStyle />
-          <ItemPanel recordInfo={this.state.data} getData={this.getData} />
-        </Styled.App>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Styled.App className="App">
+            <Styled.GlobalStyle />
+            <ItemPanel recordInfo={this.state.data} getData={this.getData} />
+          </Styled.App>
+        </ThemeProvider>
+      </BrowserRouter>
     )
   }
 }
