8370d7f60e736e52270122173e9e6642d25c48bd
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-05 17:06:54 -0700

    changed to class component and added componentDidMount lifecycle to getData #19

--- client/src/components/ItemPanel.js
+++ client/src/components/ItemPanel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import InfoBox from './InfoBox'
 import Detail from './Detail'
 
@@ -6,19 +6,27 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 import * as Styled from './styled'
 
-const ItemPanel = props => {
-  const ListView = props.recordInfo.map((record, i) => (
-    <InfoBox info={record} key={i} />
-  ))
-  const DetailView = <Detail {...props} />
-  return (
-    <Styled.ItemPanel className="itemPanel">
-      <Router>
-        <Route path="/" exact render={() => ListView} />
-        <Route path="/detail" render={() => DetailView} />
-      </Router>
-    </Styled.ItemPanel>
-  )
+class ItemPanel extends Component {
+  state = { url: '' }
+  componentDidMount() {
+    this.setState({ url: 'http://localhost:3000/inventory/' }, () => {
+      this.props.getData(this.state.url)
+    })
+  }
+  render() {
+    const ListView = this.props.recordInfo.map((record, i) => (
+      <InfoBox info={record} key={i} />
+    ))
+    const DetailView = <Detail {...this.props} />
+    return (
+      <Styled.ItemPanel className="itemPanel">
+        <Router>
+          <Route path="/" exact render={() => ListView} />
+          <Route path="/detail" render={() => DetailView} />
+        </Router>
+      </Styled.ItemPanel>
+    )
+  }
 }
 
 export default ItemPanel
