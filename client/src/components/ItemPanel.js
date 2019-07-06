aa4bc725acfdaffa10da43cd0e4b19608be799b5
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-07-06 01:59:54 -0700

    created updateURLOnClick handler #19

--- client/src/components/ItemPanel.js
+++ client/src/components/ItemPanel.js
@@ -2,31 +2,47 @@ import React, { Component } from 'react'
 import InfoBox from './InfoBox'
 import Detail from './Detail'
 
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import {
+  Route,
+  Link,
+  Switch,
+  withRouter,
+  BrowserRouter as Router
+} from 'react-router-dom'
 
 import * as Styled from './styled'
 
 class ItemPanel extends Component {
-  state = { url: '' }
+  state = { url: '', dataId: '' }
   componentDidMount() {
     this.setState({ url: 'http://localhost:3000/inventory/' }, () => {
       this.props.getData(this.state.url)
     })
   }
+  updateURLOnClick = event => {
+    console.log(this.props.history)
+    const dataId = event.target.getAttribute('data-id')
+    this.setState({ dataId: dataId }, () => {
+      this.props.history.push({
+        pathname: 'item/',
+        search: `?_${this.state.dataId}`
+      })
+    })
+  }
   render() {
     const ListView = this.props.recordInfo.map((record, i) => (
-      <InfoBox info={record} key={i} />
+      <InfoBox updateURLOnClick={this.updateURLOnClick} info={record} key={i} />
     ))
     const DetailView = <Detail {...this.props} />
     return (
       <Styled.ItemPanel className="itemPanel">
-        <Router>
+        <Switch>
           <Route path="/" exact render={() => ListView} />
-          <Route path="/detail" render={() => DetailView} />
-        </Router>
+          <Route path="/item" render={() => DetailView} />
+        </Switch>
       </Styled.ItemPanel>
     )
   }
 }
 
-export default ItemPanel
+export default withRouter(ItemPanel)
