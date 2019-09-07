92f85d13fad1d54a8c3377b79b26842ea4bf941a
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-06 17:40:39 -0700

    converted Detail component to class component #21

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,33 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import * as Styled from './styled'
 
-const Detail = props => {
-  const { recordInfo } = props
-  const path = 'http://localhost:3000/'
-  return (
-    <Styled.DetailBox className="detailBox" key={recordInfo._id}>
-      <Styled.LeftBox className="left">
-        <img src={path + recordInfo.filename} alt="" />
-      </Styled.LeftBox>
-      <Styled.RightBox className="right">
-        <p>{recordInfo.artist}</p>
-        <p>{recordInfo.title}</p>
-        <p>
-          {recordInfo.mediaCondition} / {recordInfo.coverCondition}
-        </p>
-        <p>${recordInfo.price}usd</p>
-        <p>{recordInfo.comment}</p>
-        <Styled.AddToCartButton>
-          <button onClick={props.closeOnClick}>Add To Cart</button>
-        </Styled.AddToCartButton>
-        <Styled.DetailButtonContainer>
-          <button onClick={props.closeOnClick}>Close</button>
-        </Styled.DetailButtonContainer>
-      </Styled.RightBox>
-    </Styled.DetailBox>
-  )
+class Detail extends Component {
+  render() {
+    const { recordInfo } = this.props
+    const path = 'http://localhost:3000/'
+    return (
+      <Styled.DetailBox className="detailBox" key={recordInfo._id}>
+        <Styled.LeftBox className="left">
+          <img src={path + recordInfo.filename} alt="" />
+        </Styled.LeftBox>
+        <Styled.RightBox className="right">
+          <p>{recordInfo.artist}</p>
+          <p>{recordInfo.title}</p>
+          <p>
+            {recordInfo.mediaCondition} / {recordInfo.coverCondition}
+          </p>
+          <p>${recordInfo.price}usd</p>
+          <p>{recordInfo.comment}</p>
+          <Styled.AddToCartButton>
+            <button onClick={this.props.closeOnClick}>Add To Cart</button>
+          </Styled.AddToCartButton>
+          <Styled.DetailButtonContainer>
+            <button onClick={this.props.closeOnClick}>Close</button>
+          </Styled.DetailButtonContainer>
+        </Styled.RightBox>
+      </Styled.DetailBox>
+    )
+  }
 }
 
 export default withRouter(Detail)
