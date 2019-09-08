bb5bb457dfdc79a3391d6a7b22be843de9b9418c
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-07 23:27:52 -0700

    Fetch cart items #23

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Cart extends Component {
-  state = {}
+  state = {
+    id: '',
+    artist: '',
+    title: '',
+    format: '',
+    price: '',
+    filename: ''
+  }
+
+  getData = async url => {
+    try {
+      const res = await axios.get('http://localhost:5000/cart/')
+      const { data } = await res
+      console.log(data)
+      // this.setState({
+      //   data: data.data.length > 1 ? data.data.reverse() : data.data
+      // })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
   render() {
     return <div>Cart</div>
   }
