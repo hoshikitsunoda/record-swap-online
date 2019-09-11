786e3199475c3899206fd46b0cbf4418deca6196
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-11 16:14:20 -0700

    changed upload directory location to client/public/uploads #25

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.static(path.join(__dirname, '../public/uploads')))
+app.use(express.static(path.join(__dirname, '../client/public/uploads')))
 app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
