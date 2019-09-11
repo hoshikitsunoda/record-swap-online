099433d941f2d410084bad7834307e5e47610f2c
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-11 16:16:06 -0700

    created Header component #25

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = props => {
+  return (
+    <div>
+      <div className="container">
+        <div className="left"></div>
+        <div className="right">
+          <div className="user">
+            <img src="" alt="" />
+            <div>User Name</div>
+          </div>
+          <div className="cart">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
