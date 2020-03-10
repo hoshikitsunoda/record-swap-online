8ef5f5526dbcc5af5153ddfefa7d431d741fdf75
seiki tsunoda <liquidohr@gmail.com>
2020-03-10 01:17:42 -0700

    created PostPage component and PostForm component #27

---
+++ client/src/components/PostPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const PostPage = () => {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  )
+}
+
+export default PostPage
